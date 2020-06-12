398c16101e7dad84ca6eca9f7c83fc0f5a6e8c81
Hari Haran <hariharanvenkatesank@gmail.com>
2020-06-12 11:52:16 +0530

    Initial Commit

---
+++ CustomLoaderView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLoaderView
+//
+//  Created by Hari Haran on 12/06/20.
+//  Copyright © 2020 Hari Haran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
